5e318bcb8a2c681c9015afc04230cd85f1cb7efb
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-03-18 16:47:31 -0700

    fix: sample settings for yahoo.com

--- settings-sample.py
+++ settings-sample.py
@@ -3,7 +3,8 @@ EMAIL_HOST_USER = 'username'
 EMAIL_HOST_PASSWORD = 'password'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.yahoo.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.mail.yahoo.com'
+EMAIL_PORT = 587
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
 DEFAULT_CHARSET = 'utf8'
