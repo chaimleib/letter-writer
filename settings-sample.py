8f7050a915cfc26504f10da7249cf9d238e288c1
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-03-18 15:29:35 -0700

    add: settings-sample.py for django

--- settings-sample.py
+++ settings-sample.py
@@ -1,6 +1,7 @@
 DEFAULT_FROM_EMAIL = 'name <email@example.com>'
 EMAIL_HOST_USER = 'username'
 EMAIL_HOST_PASSWORD = 'password'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.yahoo.com'
 EMAIL_PORT = 465
